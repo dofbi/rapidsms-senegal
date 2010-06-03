42bf1b66fc600d6a248860b08d210a8e62bf9f78
alioune <alioune@alioune-laptop.(none)>
2010-06-04 00:13:39 +0200

    Fix bug in wara apps , limit len message to 140

--- apps/wara/app.py
+++ apps/wara/app.py
@@ -4,21 +4,14 @@ import models as m
 import time
 from datetime import datetime 
 from time import localtime
-# Les reponses aux enregistrements du seminaire
-OK=           "Merci d'avoir porposer un theme : Taper [wara themes] pour voir la liste des themes"
-HELP =        "wara theme [theme] data [date]"
-THEME_FAIL=   "Impossible de vous envoyer la liste ,Essayez plus tard"
-MSG_HEAD=     "Le systeme va vous envoyer un SMS toutes les %s"
 
 
 class App (rapidsms.app.App):
-    kw = Keyworder ()
-    INTERVAL=10
-
+    kw = Keyworder ()    
     def start (self):
         """Configure your app in the start phase."""
         pass
-   
+     
     def parse (self, message):
         """Parse and annotate messages in the parse phase."""
         pass
@@ -36,7 +29,7 @@ class App (rapidsms.app.App):
 
     @kw("help")
     def help (message):
-        message.respond (HELP)
+        message.respond ("wara theme <theme> date <jour mois>")
 
     @kw("wara theme (.+) date (\d\d? \d\d?)")
     def add_presentation (self , message , theme , date):
@@ -46,19 +39,15 @@ class App (rapidsms.app.App):
         try:
 	    jr  , hr  =tuple(date.split())
             an ,mois = localtime ()[0], localtime()[1]
-            m.Seminaire.objects.create (
-                theme=theme ,
-                phone =message.connection.identity,
-                date=datetime (int (an),\
-                               int (mois),\
-                               int (jr),\
-                               int (hr)))
-            message.respond (OK)
+            m.Seminaire.objects.create\
+            (theme=theme , phone =message.connection.identity,
+                    date=datetime (int (an),int (mois),int (jr),int (hr)))
+            message.respond ("Merci d'avoir proposer un theme  au seminaire ,\
+                    tapez [wara themes] pour la liste")
             return True
         except Exception , e:
             print "Exception"
             print e
-            message.respond (SAVE_FAIL)
     
 
     @kw ("wara themes|warathemes")
@@ -66,35 +55,51 @@ class App (rapidsms.app.App):
         """
 	Lister tous les themes proposes lors du seminaire WARA
         (WEST AFRICAN RESEARCH )
-	"""
-        try:
-	   seminaires  = m.Seminaire.objects.all()
-	   if not  len(seminaires):
-		message.respond ("""
-		Aucun seminaire enregistre pour le moment :
-		Tapez: wara theme [Votre theme] date [date]   
-		""")
-                return  True
-
-           message.respond (MSG_HEAD%str(self.INTERVAL))
-           for s in seminaires :
-                    blast = "Presentation sur : %s le %d a %d  heures"\
-                            %( s.theme ,\
-                               s.date.day,\
-                               s.date.hour)
-                    try:
-                        message.respond (blast)
-                        time.sleep (self.INTERVAL)
-                    except Exception ,e:
-                        print "Message trop long , peu etre"
-                        print e
-
-
-        except Exception ,e :
-            print "Exception"
-            print e
-            message.respond (THEME_FAIL)
+	"""       
+        def to_str(el):
+            return  "[Presentation: %s ,Date : le %s a %s Heures]"%\
+                        (el.theme  ,el.date.day ,  el.date.hour)
+        def _get_presentation (old_list , given_el):
+             try:
+                   
+                   item  =old_list [-1]        
+                   new_given  = to_str (item) + given_el
+                   if len (new_given)>= 140:
+                        return given_el
+                   else :
+                        old_list.pop ()
+                        return  new_given 
+                        
+             except Exception , e:
+                print "Into _get_presentation"
+                print  e
+                return  given_el
     
+
+        seminaires  = m.Seminaire.objects.all ()
+        if not seminaires  or len (seminaires)==0:
+            message.respond  ("Aucun seminaire pour le  moment")
+            return  
+        # We got some presentations into data base
+        seminaires  = list (seminaires)
+        given_el    =  seminaires.pop ()
+        given_el    = to_str (given_el)
+        while True :
+            try:
+                output_el  =_get_presentation (seminaires , given_el)
+                # Good message size 
+                if  output_el  == given_el:
+                    message.respond(output_el) 
+                    given_el  =to_str (seminaires.pop ())
+
+                    time.sleep (10)
+                else :
+                     given_el =output_el 
+            
+            except Exception , e :
+                print  e
+                break 
+                
     def cleanup (self, message):
         """Perform any clean up after all handlers have run in the
         cleanup phase."""
